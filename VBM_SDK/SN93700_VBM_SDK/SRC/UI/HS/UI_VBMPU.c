bc515f405c8913a85ea95a9ed0afbb3b6b86df79
yexinhua <yxh_sun@163.com>
2018-12-21 17:43:15 +0800

    软件把连接显示温度打开 并且添加TWC通讯失败打印

--- VBM_SDK/SN93700_VBM_SDK/SRC/UI/HS/UI_VBMPU.c
+++ VBM_SDK/SN93700_VBM_SDK/SRC/UI/HS/UI_VBMPU.c
@@ -14284,7 +14284,7 @@ void UI_RedrawStatusBar(uint16_t *pThreadCnt)
 			   	UI_UpdateBarIcon_Part1();
 				UI_UpdateBarIcon_Part2();	
 				UI_VolBarDisplay(ubGetVoiceTemp);
-				//UI_TempBarDisplay(ubRealTemp);
+				UI_TempBarDisplay(ubRealTemp);
 				if(ubPairOK_SwitchCam == 1)
 				{
 					UI_DisableScanMode();
@@ -15173,7 +15173,11 @@ void UI_RecvBURequest(TWC_TAG tRecv_StaNum, uint8_t *pTwc_Data)
             UI_CamNum_t tCamNum = NO_CAM;
             
             APP_TwcTagMap2CamNum(tRecv_StaNum, tCamNum);
-            if(pTwc_Data[1] == UI_TEMP_CHECK && (pTwc_Data[7] != (uint8_t) pTwc_Data[2] + pTwc_Data[3] + pTwc_Data[4] + pTwc_Data[5] + pTwc_Data[6])) break;
+            if(pTwc_Data[1] == UI_TEMP_CHECK && (pTwc_Data[7] != (uint8_t) pTwc_Data[2] + pTwc_Data[3] + pTwc_Data[4] + pTwc_Data[5] + pTwc_Data[6])) 
+            {
+                printd(1,"TWC checkflag Fail\n");
+                break;
+            }
             if (tUiReportMap2Func[pTwc_Data[UI_REPORT_ITEM]].pvAction)
                 tUiReportMap2Func[pTwc_Data[UI_REPORT_ITEM]].pvAction(tCamNum, (uint8_t *)(&pTwc_Data[UI_REPORT_DATA]));
             break;
